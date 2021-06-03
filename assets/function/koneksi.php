5d4a506f16e76814edcac8cba6b6c1d7e824ed6c
Muhamad Rifqi <muhamadrifqi1000@gmail.com>
2021-06-04 06:03:38 +0700

    Update koneksi.php

--- assets/function/koneksi.php
+++ assets/function/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "educov";
+	$server = "remotemysql.com";
+	$username = "qH32rdB2lf";
+	$password = "m2oRYSdKRS";
+	$database = "qH32rdB2lf";
 	
 	$koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal");
 	

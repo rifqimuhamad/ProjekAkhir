f2284ae5126c2f8c97c4e5c177967487999d1d1e
Muhamad Rifqi <muhamadrifqi1000@gmail.com>
2021-06-07 14:57:26 +0700

    Update helper.php

--- assets/function/helper.php
+++ assets/function/helper.php
@@ -1,20 +1,10 @@
 <?php
 
 	define("base_url", "https://projekweb.herokuapp.com/");
-	define("base_urls", "https://projekweb.herokuapp.com/store.php");
 
-	$arrayStatusPesanan[0] = "Menunggu Pembayaran";
-	$arrayStatusPesanan[1] = "Pembayaran Sedang Di Validasi";
-	$arrayStatusPesanan[2] = "Lunas";
-	$arrayStatusPesanan[3] = "Pembayaran Di Tolak";
 	
 	function rupiah($nilai = 0){
 		$string = "Rp," . number_format($nilai);
-		return $string;
-	}
-
-	function jums($nilai = 0){
-		$string =number_format($nilai);
 		return $string;
 	}
 	
@@ -30,9 +20,33 @@
 						
 						while($row=mysqli_fetch_assoc($query)){
 							if($kategori_id == $row['kategori_id']){
-								$string .= "<li><a href='".base_url."store.php?kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
+								$string .= "<li><a href='".base_url."?page=mainstore&kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
 							}else{
-								$string .= "<li><a href='".base_url."store.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
+								$string .= "<li><a href='".base_url."?page=mainstore&kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
+							}
+						}
+				
+				$string .= "</ul>";
+			
+			$string .= "</div>";		
+			
+			return $string;
+	}
+	function kategorib($kategori_id = false){
+		global $koneksi;
+		
+		$string = "<div id='menu-kategori'>";
+			$string .= "<h3>Category</h3><hr />";
+			
+				$string .= "<ul>";
+					
+						$query = mysqli_query($koneksi, "SELECT * FROM kategori_blog WHERE status='on'");
+						
+						while($row=mysqli_fetch_assoc($query)){
+							if($kategori_id == $row['kategori_blog_id']){
+								$string .= "<li><a href='".base_url."?page=blog&kategori_blog_id=$row[kategori_blog_id]' class='active'>$row[kategori_blog]</a></li>";
+							}else{
+								$string .= "<li><a href='".base_url."?page=blog&kategori_blog_id=$row[kategori_blog_id]'>$row[kategori_blog]</a></li>";
 							}
 						}
 				
